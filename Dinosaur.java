4a62e4bc07fa9556ad0b2cc1915bbf078b55f3dd
su7163if <stuff@stuff.com>
2017-02-15 17:37:53 -0600

    added dinosaur Triceratops and added roar method.

---
+++ Dinosaur.java
@@ -0,0 +1,16 @@
+package Richard;
+
+/**
+ * Created by su7163if on 2/15/2017.
+ */
+
+public class Dinosaur {
+
+
+        public void roar(){
+            System.out.println("GRRR!!!!!");
+        }
+
+
+
+}
